e3a4753feb63917b6c7ef84c4783a30ae57bb5b4
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-04-25 22:52:58 +0900

    Use dotenv

--- app/constants.js
+++ app/constants.js
@@ -1,12 +1,17 @@
 // @flow
 
+require('dotenv').config({ path: __dirname + '/../.env' })
+
+let clientId = process.env.CLIENT_ID;
+let clientSecret = process.env.CLIENT_SECRET;
+
 export default {
   AUTH_SCOPE: ['user:email', 'notifications', 'repo'],
 
   DEFAULT_AUTH_OPTIONS: {
     hostname: 'github.com',
-    clientId: '',
-    clientSecret: '',
+    clientId: clientId,
+    clientSecret: clientSecret,
   },
 
   REPO_SLUG: 'h-michael/ghn',
